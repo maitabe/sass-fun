c8756aa41942d22b887e520a3887db52a09b6f65
maitabe <martushb@gmail.com>
2016-12-30 18:08:06 +0200

    main files project

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var install = require('gulp-install');
+
+gulp.task('connect', function() {
+  connect.server();
+});
+
+gulp.task('sass', function () {
+  return gulp.src('*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('sass'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('*.scss', ['sass']);
+});
+
+gulp.task('install', function () {
+  gulp.src(['./package.json'])
+  .pipe(install());
+});
+
+gulp.task('default', ['install', 'sass:watch', 'connect']);
